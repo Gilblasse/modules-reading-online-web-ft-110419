a6ce79c9407a267eb0203fa3181f3008e891a7e3
Nethelbert <gilblasse@gmail.com>
2019-11-27 15:47:42 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 module MetaDancing
  
   def metadata
     "This class produces objects that love to dance."
   end
 end
+=======
+module 
+>>>>>>> cfcb95b58b3047b4e4fbe58bc61e9d8c08c7f57a
